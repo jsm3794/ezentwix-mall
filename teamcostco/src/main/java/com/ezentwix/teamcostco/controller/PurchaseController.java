5adeb3a292f0a267f83b4a281ea8c94ffebfe445
sangmin <j__sm@naver.com>
2024-09-23 14:21:54 +0900

    코드 정리

---
+++ teamcostco/src/main/java/com/ezentwix/teamcostco/controller/PurchaseController.java
@@ -0,0 +1,8 @@
+package com.ezentwix.teamcostco.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PurchaseController {
+
+}
---
+++ teamcostco/src/main/java/com/ezentwix/teamcostco/service/PurchaseService.java
@@ -0,0 +1,8 @@
+package com.ezentwix.teamcostco.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PurchaseService {
+
+}
