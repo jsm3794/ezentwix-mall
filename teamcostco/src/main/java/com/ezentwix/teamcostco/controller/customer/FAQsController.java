3e1a3a54cc1bc7e2d9e7627cd82b59f0f03cb839
Oh-hyejun <qwegw0303@naver.com>
2024-09-27 11:12:19 +0900

    FAQ 진행중 .. controller DB 설계

--- teamcostco/src/main/java/com/ezentwix/teamcostco/controller/customer/FAQsController.java
+++ teamcostco/src/main/java/com/ezentwix/teamcostco/controller/customer/FAQsController.java
@@ -3,21 +3,29 @@ package com.ezentwix.teamcostco.controller.customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ezentwix.teamcostco.service.FAQsService;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/customer/FAQs")
 public class FAQsController {
     private final FAQsService faqsService;
 
-    @GetMapping("/customer/FAQs")
-    public String showCustomerMain(Model model) {
+    @GetMapping("")
+    public String showFAQs(Model model) {
         faqsService.configureModel(model);
         return "index";
     }
 
-
+    @GetMapping("/detail/{id}")
+    public String showFAQDetail(@RequestParam String param) {
+        return "index";
+    }
+    
 }
